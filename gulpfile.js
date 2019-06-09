008e940148cb90685ad8630db34dafa17d5e0c77
Viktoriya <vika-terleckaya@mail.ru>
2019-06-10 00:39:21 +0300

    gulpgit add .!

---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+const { src, dest, task, series, watch, parallel } = require('gulp');
+const rm = require('gulp-rm');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
+const px2rem = require('gulp-smile-px2rem');
+const cleanCss = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const env = process.env.NODE_ENV;
+
+
+sass.compiler = require('node-sass');
+
+task('clean', () => {
+  return src('./dist/**/*', { read: false })
+    .pipe(rm())
+});
+
+task('copy:html', () => {
+  return src('./*.html')
+    .pipe(dest('./dist'))
+    .pipe(reload({ stream: true }))
+});
+
+const styles = [
+  './node_modules/normalize.css/normalize.css',
+  './entry.scss'
+
+];
+
+task('styles', () => {
+  return src(styles)
+    .pipe(gulpIf(env === 'dev', sourcemaps.init()))
+    .pipe(concat('entry.scss'))
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(px2rem())
+    .pipe(gulpIf(env === 'dev', autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    })
+    )
+    )
+    
+    .pipe(gulpIf(env === 'prod', cleanCss()))
+    .pipe(gulpIf(env === 'dev', sourcemaps.write()))
+    .pipe(dest('dist'))
+    .pipe(reload({ stream: true }));
+});
+
+
+
+
+
+task('scripts', () => {
+  return src('./script.js')
+  .pipe(gulpIf(env === 'dev', sourcemaps.init()))
+    .pipe(concat('script.js', { newLine: ";" }))
+    .pipe(
+      babel({
+        presets:['@babel/env']
+      })
+    )
+    .pipe(gulpIf(env === 'prod', uglify()))
+    .pipe(gulpIf(env === 'dev', sourcemaps.write()))
+    .pipe(dest('dist'))
+    .pipe(reload({ stream: true }));
+});
+
+task('server', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    },
+    open: false
+  });
+});
+
+task('watch', () => {
+  watch('./*.html', series('copy:html')),
+  watch('./css/**/*.scss', series('styles')),
+  watch('./js/*.js', series('scripts'))});
+ 
+
+
+task('default',
+ series(
+   'clean', 
+   parallel('copy:html', 'styles', 'scripts'),
+   parallel('watch','server'))
+);
+task('build',
+ series(
+   'clean', 
+   parallel('copy:html', 'styles', 'scripts'),
+   
+)
+);
+
